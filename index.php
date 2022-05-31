782560a2aaa8c59d3be685c172807a77b6738f17
Michael <103090272+Michael-bilhu@users.noreply.github.com>
2022-05-31 17:41:24 +0000

    changes

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <html>
   <head>
-    <title>PHP Test Page</title>
+    <title>World's smallest from Handler</title>
   </head>
   <body>
     <h1>World's smallest from Handler</h1>
   <?php
   if(isset($_POST["FirstName"])){//show data
-    echo $_POST["FirstName"]
+    echo $_POST["FirstName"];
   }else{//show form
     echo '
          <form method ="post" action="">
-         <p>First Name: <Input type="text"name 
-         ="FirstName"</p>
+         <p>First Name: <input type="text"name="FirstName" /</p>
           <p><input type="Submit" /></p>
          </form>
 
