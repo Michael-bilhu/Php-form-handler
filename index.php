613cb74c4576a555a864d29014b77995e2609938
Michael-bilhu <Michaelbilhu@gmail.com>
2022-06-01 00:05:26 -0700

    adds bootstrap designs

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <h1>World's smallest from Handler</h1>
   <?php
   if(isset($_POST["FirstName"])){//show data
-    echo $_POST["FirstName"]
+    echo $_POST["FirstName"];
   }else{//show form
     echo '
          <form method ="post" action="">
