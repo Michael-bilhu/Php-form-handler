a456089d1de40123f294902c75996ff2fe39ed91
Michael-bilhu <Michaelbilhu@gmail.com>
2022-05-31 23:54:24 -0700

    Adds bootstrap design

--- index.php
+++ index.php
@@ -3,10 +3,27 @@
     <title>PHP Test Page</title>
   </head>
   <body>
-    <h1>PHP Test Page -can you see this!</h1>
-    <?php
-    echo '<p>This is PHP!</p>';
-    ?>
-    <p> Hello from Github </p>
+    <h1>World's smallest from Handler</h1>
+  <?php
+  if(isset($_POST["FirstName"])){//show data
+    echo $_POST["FirstName"]
+  }else{//show form
+    echo '
+         <form method ="post" action="">
+         <p>First Name: <Input type="text"name 
+         ="FirstName"</p>
+          <p><input type="Submit" /></p>
+         </form>
+
+         ';
+  }
+
+?>
+
+
+
+
+    
+ 
   </body>
 </html>
