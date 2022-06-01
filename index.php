a6096128661a8b7b6cc5fff81d9fa266051a6fe7
Michael-bilhu <Michaelbilhu@gmail.com>
2022-06-01 00:03:08 -0700

    changes

--- index.php
+++ index.php
@@ -1,25 +1,16 @@
 <html>
   <head>
-    <title>World's smallest from Handler</title>
+    <title>PHP Test Page</title>
   </head>
   <body>
     <h1>World's smallest from Handler</h1>
   <?php
   if(isset($_POST["FirstName"])){//show data
-<<<<<<< HEAD
     echo $_POST["FirstName"]
   }else{//show form
     echo '
          <form method ="post" action="">
-         <p>First Name: <Input type="text"name 
-         ="FirstName"</p>
-=======
-    echo $_POST["FirstName"];
-  }else{//show form
-    echo '
-         <form method ="post" action="">
-         <p>First Name: <input type="text"name="FirstName" /</p>
->>>>>>> 782560a2aaa8c59d3be685c172807a77b6738f17
+         <p>First Name: <Input type="text"name ="FirstName"</p>
           <p><input type="Submit" /></p>
          </form>
 
